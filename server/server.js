602ddab826831007aba18740e498f9a063dd3ebd
modanwal-pg <modanwal.d@pg.com>
2025-11-04 18:21:03 +0530

    backend optimization

--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const path = require("path");
-const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
 
 dotenv.config();
 
@@ -19,15 +17,6 @@ app.use(cors({
 
 app.use(express.json());
 
-app.use(helmet());
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, 
-  max: 100,                
-  message: { success: false, msg: "Too many requests. Try again later." }
-});
-app.use(limiter);
-
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/topics', require('./routes/topicRoutes'));
 app.use('/api/problems', require('./routes/problemRoutes'));
@@ -40,4 +29,5 @@ app.get(/.*/, (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
