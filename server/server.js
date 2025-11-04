2535fd4e063070400451fd4b05192b1500fc8347
modanwal-pg <modanwal.d@pg.com>
2025-11-04 17:12:32 +0530

    port changed to 8080

--- server/server.js
+++ server/server.js
@@ -31,6 +31,6 @@ app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
